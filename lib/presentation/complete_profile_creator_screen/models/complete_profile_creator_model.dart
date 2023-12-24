2a6cd6a3beb67d2a7f61a04cb945b67b72f76baa
Godson <gnqnpn@gmail.com>
2023-12-24 21:20:29 +0100

    some changes

--- lib/presentation/complete_profile_creator_screen/models/complete_profile_creator_model.dart
+++ lib/presentation/complete_profile_creator_screen/models/complete_profile_creator_model.dart
@@ -1,25 +1,22 @@
 /// This class defines the variables used in the [complete_profile_creator_screen],
 /// and is typically used to hold data that is passed between different parts of the application.
 import 'package:get/get.dart';
+import 'package:iynfluencer/data/models/Influencer/influencer_response_model.dart';
 
 class CompleteProfileCreatorModel {
-  String bio;
-  List<String> niches;
-  List<User>? user;// Update this to be a list of strings
-   List<Social> socials;
+  String? bio;
+  List<String>? niches;
+  List<User>? user; // Update this to be a list of strings
+  List<Social>? socials;
 
-  CompleteProfileCreatorModel({
-    required this.bio,
-    required this.niches,
-    required this.user,
-  });
+  CompleteProfileCreatorModel({this.bio, this.niches, this.user, this.socials});
 
   Map<String, dynamic> toJson() {
     return {
       'bio': bio,
       'niche': niches,
-      'user': user.map((user) => user.toJson()).toList(),
-      'social': socials.map((social) => social.toJson()).toList(),
+      'user': user!.map((user) => user.toJson()).toList(),
+      'social': socials!.map((social) => social.toJson()).toList(),
     };
   }
 }
@@ -40,5 +37,6 @@ class Social {
       'name': name,
       'followers': followers,
       'url': url,
+    };
   }
 }
