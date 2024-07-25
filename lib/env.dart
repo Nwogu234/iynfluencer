b564a023659a0f7632cc24bb4b76b9dfabe1121a
Emmanism <emmanism458@gmail.com>
2024-07-25 20:51:12 +0100

    cleaning

--- lib/env.dart
+++ lib/env.dart
@@ -1,16 +1,1 @@
-/* 
-const stripeSecretKey =
-    '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';
 
-const stripePublishableKey =
-    'pk_live_51PCjgT08jEBjxNSB6X9ckOyaHswgeoIzuM061Dy8UPgUMzu5znkSiIow5Y6mQctl75BKsLaGvs2vOM2bva3Cf4Cd00yFRz9LwI'; */
-
-const stripeSecretKey =
-    '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';
-
-const stripePublishableKey =
-    'pk_live_51ONc6lDB4UoLMG6RcTkmeV46fLNDvl9FiK3HXUz5zyKe6Ufym0IUQtwU4gCFjrJ9Jymz7BEXx6RYMRdh1mNSJE1z000Qg1Vw3M';
-
-const appId = '3afeeda7-bb93-4b0b-807a-4ed6bb727e4c';
-
-const signalKey = 'Mzc1M2ZiOTMtNDkwNC00MGUxLWEyMzQtNjI2YjA4ZWFhMWYy';
